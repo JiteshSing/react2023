25b15731a07537347279e25a6f853384dfe3753f
Jitesh Singh <125820822+JiteshSing@users.noreply.github.com>
2023-02-20 16:32:18 +0530

    Add files via upload
    uncontrolled component example

---
+++ leapun.js
@@ -0,0 +1,27 @@
+import { useRef, useState } from "react"
+
+function Leapun(){
+    const y = useRef('');
+    const[a, setA] = useState(0);
+    let leapyr = (e) =>{
+         let r;
+        let c= parseInt(y.current.value);
+        if((c%4 == 0) && (c%100 !==0) ||(c%400 == 0)){
+           r = "It's leap Year";
+        }else{
+           r = "It's not a leap year"
+        }
+        setA(r);
+        e.preventDefult()
+    }
+
+    return(<div>
+        <h3>Enter Year to Check Leap Year</h3>
+        <input type="text" ref={y} placeholder="Enter a year"/>
+        <br/>
+        <input type="button" value="Click" onClick={leapyr}/>
+        <br/>
+        <p>{a}</p>
+    </div>)
+}
+export default Leapun;
